61f6db00cb47a87456e939e0fbab531826f702df
MinSyeoni <33422760+MinSyeoni@users.noreply.github.com>
2018-09-14 16:55:24 +0900

    run ani

--- character_move.py
+++ character_move.py
@@ -1,19 +1,17 @@
 from pico2d import *
 
 open_canvas()
-
 grass = load_image('grass.png')
-characrer = load_image('character.png')
-
+character = load_image('run_animation.png')
 x = 0
+frame = 0
 while (x < 800):
     clear_canvas()
-    grass.draw(400,30)
-    characrer.draw(x,90)
+    grass.draw(400, 30)
+    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
     update_canvas()
-    x=x+2
-    delay(0.01)
-
-#fill here
-
+    frame = (frame + 1) % 8
+    x += 5
+    delay(0.05)
+    get_events()
 close_canvas()
