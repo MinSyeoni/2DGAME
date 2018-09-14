5753cc0412c3f3c277e85b76967f99317ad95f05
MinSyeoni <33422760+MinSyeoni@users.noreply.github.com>
2018-09-14 16:50:43 +0900

    move

--- character_move.py
+++ character_move.py
@@ -1,4 +1,19 @@
-import turtle
+from pico2d import *
 
-turtle.forward(100)
-turtle.exitonclick()
+open_canvas()
+
+grass = load_image('grass.png')
+characrer = load_image('character.png')
+
+x = 0
+while (x < 800):
+    clear_canvas()
+    grass.draw(400,30)
+    characrer.draw(x,90)
+    update_canvas()
+    x=x+2
+    delay(0.01)
+
+#fill here
+
+close_canvas()
