43e882b003fea0176fb12c0a952ad03ff82af2ea
MinSyeoni <33422760+MinSyeoni@users.noreply.github.com>
2018-10-10 20:34:37 +0900

    캐릭터 애니메이션 방향별로 적용

--- term/tutorial_state.py
+++ term/tutorial_state.py
@@ -16,12 +16,23 @@ class Player:
         self.y = random.randint(100,500)
         self.speed = random.uniform(1.0,3.0)
         self.frame = random.randint(0,7)
+        self.goto = 0 # 0 업 1 다운
         self.image = load_image('image/run_stand_ani.png')
-        self.state = 0 # 0 - 멈춤, 1- 왼쪽, 2 - 오른쪽, 3 - 위, 4 - 아래
+        self.state = 0 # 0 왼쪽 1 오른쪽 2 위 3 아래
+        self.idle = 0 # 0 이동중 1 왼쪽  2 오른쪽
         print(self.image)
 
     def draw(self):
-        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+        if self.state == 0 or (self.idle == 1 and self.goto != 2):
+            self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        elif self.state == 1 or (self.idle == 2 and self.goto != 2) :
+            self.image.clip_draw(self.frame * 100, 100, 100, 100, self.x, self.y)
+        elif self.idle == 1:
+            self.image.clip_draw(self.frame * 100, 200, 100, 100, self.x, self.y)
+        elif self.idle == 2:
+            self.image.clip_draw(self.frame * 100, 300, 100, 100, self.x, self.y)
+        else:
+            self.image.clip_draw(self.frame*100, self.state * 100, 100, 100, self.x, self.y)
 
     def update(self):
         events = get_events()
@@ -42,22 +53,24 @@ def draw():
 def update():
     global player
     player.update()
-    if player.state == 1:
+    if player.state == 0:
         player.x -= 5
-        if player.x < 100: # 이동범위 제한
+        if player.x < 100:
             player.x = 100
-    elif player.state == 2:
+    elif player.state == 1:
         player.x += 5
         if player.x >700:
             player.x = 700
-    elif player.state == 3:
+
+    if player.goto == 0:
         player.y += 5
         if player.y > 520:
             player.y = 520
-    elif player.state == 4:
+    elif player.goto == 1:
         player.y -= 5
         if player.y < 150:
             player.y = 150
+
 
     delay(0.05)
 
@@ -77,15 +90,21 @@ def handle_events():
 
         if event.type == SDL_KEYDOWN:
             if event.key == SDLK_a:     ##왼쪽
-                player.state = 1
+                player.state = 0
             elif event.key == SDLK_d:   ##오른쪽
-                player.state = 2
+                player.state = 1
             elif event.key == SDLK_w:   ##위
-                player.state = 3
+                player.goto = 0
             elif event.key == SDLK_s:   ##아래
-                player.state = 4
-        elif event.type == SDL_KEYUP:
-            player.state = 0
+                player.goto = 1
+        elif event.type == SDL_KEYUP: # 키 안누를때 앉기
+            if event.key == SDLK_a:  ##왼쪽
+                player.idle = 1
+            elif event.key == SDLK_d:  ##오른쪽\
+                player.idle = 2
+            player.state = 2
+            player.goto = 2
+
 
 def exit():
     close_canvas()
