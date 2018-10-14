9f9f31ebc295b0c42ba1e421edc8b7cbfe354da6
Min Su Yeon <alstndus0726@naver.com>
2018-10-14 19:21:21 +0900

    상점씬 추가

---
+++ term/store_state.py
@@ -0,0 +1,28 @@
+from pico2d import *
+from term import game_framework
+from term import tutorial_state
+
+class Store:
+    def __init__(self):
+        self.image = load_image('image/background.png')
+        print(self.image)
+    def draw(self):
+        self.image.draw(400, 300)
+
+def enter():
+    global store
+    store = Store()
+
+def draw():
+    global store
+    clear_canvas()
+    
+    store.draw()
+
+    update_canvas()
+
+def exit():
+    close_canvas()
+
+if __name__ == '__main__':
+    main()
