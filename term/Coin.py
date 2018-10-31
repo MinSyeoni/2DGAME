f503c5b7894a1b4fee177236d987eb849fe660cb
Min Su Yeon <alstndus0726@naver.com>
2018-10-31 22:26:45 +0900

    폰트추가

--- term/Coin.py
+++ term/Coin.py
@@ -3,9 +3,12 @@ from pico2d import *
 class Coin:
     def __init__(self):
         self.image = load_image('image/coin.png')
+        self.coin = 100
+        self.font = load_font('resource/ConsolaMalgun.TTF', 40)
 
     def draw(self):
         self.image.draw(600,550)
+        self.font.draw(650, 550, '%d ' % (self.coin),) #코인 출력
 
     def update(self):
         pass
--- term/Life.py
+++ term/Life.py
@@ -3,9 +3,12 @@ from pico2d import *
 class Life:
     def __init__(self):
         self.image = load_image('image/life.png')
+        self.life = 3
+        self.font = load_font('resource/ConsolaMalgun.TTF', 40)
 
     def draw(self):
-        self.image.draw(550,550)
+        self.image.draw(500,550)
+        self.font.draw(530, 550, '%d ' % (self.life))  # 생명 출력
 
     def update(self):
         pass
