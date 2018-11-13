40aabdf33c4b9c178af98c9cf909c6607c116877
Min Su Yeon <alstndus0726@naver.com>
2018-11-13 23:20:51 +0900

    title sound완성

--- term/title_state.py
+++ term/title_state.py
@@ -17,9 +17,21 @@ class title:
     def update(self):
         pass
 
+class key:
+    def __init__(self):
+        self.timer = 0
+        self.key_sound = load_wav('resource/button.wav')
+        self.key_sound.set_volume(100)
+    def draw(self):
+        pass
+    def update(self):
+        self.timer += 0.1
+        print(self.timer)
+
 def enter():
-    global bg
+    global bg,button
     bg = title()
+    button = key()
 
 def draw():
     global bg
@@ -34,6 +46,8 @@ def exit():
     pass
 
 def handle_events():
+    global bg,button
+    i = 0
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -42,7 +56,16 @@ def handle_events():
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
             elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
-                game_framework.change_state(tutorial_state)
+                button.key_sound.play()
+                while i >= 0:
+                    # print(i)
+                    i += 0.0001
+                    if i > 17:
+                        i = 17
+                        break
+                if i == 17:
+                    game_framework.change_state(tutorial_state)
+
 
 if __name__ == '__main__':
     main()
