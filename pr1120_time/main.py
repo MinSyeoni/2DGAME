b8bf8c655f361cb5beb9c6b1024c0a9058b0c425
Min Su Yeon <alstndus0726@naver.com>
2018-12-05 13:43:08 +0900

    pr1120

--- pr1120_time/main.py
+++ pr1120_time/main.py
@@ -1,6 +1,15 @@
+import platform
+import os
+
+if platform.architecture()[0] == '32bit':
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x86"
+else:
+    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x64"
+
 import game_framework
 import main_state
 from pico2d import *
+
 
 open_canvas()
 game_framework.run(main_state)
